976b713e44431d1bcac8b4fd46d320ce0823f02d
wnsdyd1012 <wnsdyd1012@gmail.com>
2023-08-10 13:36:57 +0900

    token 유지
    token 유지

--- frontend/login.js
+++ frontend/login.js
@@ -1,7 +1,5 @@
 const form = document.querySelector("#login-form");
 
-let accessToken = null;
-
 const handleSubmit = async (event) => {
   event.preventDefault();
   const formData = new FormData(form);
@@ -13,32 +11,15 @@ const handleSubmit = async (event) => {
     body: formData,
   });
   const data = await res.json();
-  accessToken = data.access_token;
+  const accessToken = data.access_token;
+  window.localStorage.setItem("token", accessToken);
 
-  const infoDiv = document.querySelector("#info");
-  infoDiv.innerText = "로그인 되었습니다!";
-
-  window.location.pathname = "/";
-
-  // const btn = document.createElement("button");
-  // btn.innerText = "상품 가져오기";
-  // btn.addEventListener("click", async () => {
-  //   const res = await fetch("/items", {
-  //     headers: {
-  //       Authorization: `Bearer ${accessToken}`,
-  //     },
-  //   });
-  //   const data = await res.json();
-  //   console.log(data);
-  // });
-  // infoDiv.appendChild(btn);
-
-  // if (res.status === 200) {
-  //   alert("로그인 성공!");
-  //   window.location.pathname = "/";
-  // } else if (res.status === 401) {
-  //   alert("id 혹은 password가 틀렸습니다.");
-  // }
+  if (res.status === 200) {
+    alert("로그인 성공!");
+    window.location.pathname = "/";
+  } else if (res.status === 401) {
+    alert("id 혹은 password가 틀렸습니다.");
+  }
 };
 
 form.addEventListener("submit", handleSubmit);
